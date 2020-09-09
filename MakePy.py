bdd92d16833f47854a55b1e3b5796b40177eda12
Gavin <gavinbrrtt@gmail.com>
2020-09-09 14:34:03 -0700

    Fixed initial MakePy functionality.

--- MakePy.py
+++ MakePy.py
@@ -1,9 +1,13 @@
-from os import path
-from sys import argv, exit
-from glob import glob
+#!/usr/bin/env python3
+
 import re
+from os import path
+from glob import glob
+from sys import argv, exit
+
 
 class FileObject:
+	
 	def __init__(self, name, abspath, relpath, objname, depends, main):
 		self.name = name
 		self.abspath = abspath
@@ -13,9 +17,8 @@ class FileObject:
 		self.main = main
 
 
-def make_makefile(CC, CFLAGS, STD, CLEAN, OUT, FOBJS):
-	LINKS = ''
-	with open('Makefile', 'w') as outfile:
+def make_makefile(CC, CFLAGS, STD, CLEAN, OUT, FOBJS, PATH):
+	with open(f'{PATH}Makefile', 'w') as outfile:
 		# write compiler variables
 		outfile.write(f'{CC}\n{CFLAGS}\n{STD}\nOBJ = {" ".join([f.objname for f in FOBJS])}\n\n')		
 
@@ -30,17 +33,20 @@ def make_makefile(CC, CFLAGS, STD, CLEAN, OUT, FOBJS):
 
 		# write out cleaning rul
 		outfile.write(CLEAN);
+	print(f'Makefile generated for {OUT}')
 
 
-def set_makefile(compiler, outfile, fobjs):
+def set_makefile(compiler, outfile, fobjs, path):
 	# set compiler variables
 	CC = f'CC = {compiler}'
 	CFLAGS = f'CFLAGS = -Wall -Werror'
 	STD = 'STD = -std=c11'
 	CLEAN = f'clean:\n\trm {argv[1]} *.o'
+	if (path[-1] != '/'):
+		path += '/'
 
 	# generate makefile
-	make_makefile(CC,CFLAGS,STD,CLEAN,outfile,fobjs)
+	make_makefile(CC,CFLAGS,STD,CLEAN,outfile,fobjs,path)
 
 
 def grab_files(directory):
@@ -56,54 +62,97 @@ def grab_files(directory):
 	h_files = list(filter(h_reg.search, files))
 	c_files = list(filter(c_reg.search, files))
 	cpp_files = list(filter(cpp_reg.search, files))
-	
 	return h_files, c_files, cpp_files
 
 
-def build_map(line, deps, main):
+def build_map(line, dependencies, main):
+	# check if file has dependencies
 	if (line[0] == "#include"):
 		if (line[1][0] == '<'):
-			deps.append(line[1].strip('<>\n'))
+			dependencies.append(line[1].strip('<>\n'))
 		if (line[1][0] == '"'):
-			deps.append(line[1].strip('"\n'))
-			
-	# file is a main file
+			dependencies.append(line[1].strip('"\n'))
+	# FIXME: check for multiple main functions
+	# check if file has a main function
 	if (line[0] == "int" and re.match('main\(.*\)', line[1])):
-		#print('main file')
 		main = True
 
 
-def get_dependencies(h, c, cpp):
+def determine_regex(compiler):
+	return re.compile('\.c$') if compiler == 'gcc' else re.compile('(\.cc$)|(\.c\+\+$)|(\.cpp$)')
+
+
+def determine_file_type(compiler, c, cpp):
+	return c if compiler == 'gcc' else cpp
+
+
+def get_dependencies(compiler, h, c, cpp):
+	
 	files = []
-	for cf in c:
-		f = open(cf, 'r')
-		lines = [e.split(' ') for e in f.readlines()]
-		deps = []
-		main = False
-		for line in lines:
-			build_map(line, deps, main)
-		regex = re.compile('\.c$')
-		c_f = list(filter(regex.search, cf.split('/')))[0]
-		rel = cf.split('/')
-		rel.pop()
-		rel = '/'.join(rel)
-		print(c_f)
-		of = re.sub('\.c$', '.o', c_f)
-		ef = FileObject(c_f, cf, rel, of, deps, main)
-		print(f'Mapping {ef.name}')
-		files.append(ef)
-		f.close()
+	# get the regex for the file type
+	regex = determine_regex(compiler)
+
+	# determine whether to iterate through c or cpp files
+	c_files = determine_file_type(compiler, c, cpp)
+
+	# iterate through all files
+	for c_file in c_files:
+		with open(c_file, 'r') as f:
+			dependencies = []
+			main = False
+			lines = [lines.split(' ') for lines in f.readlines()]
+			
+			# check line for dependencies and main functions
+			for line in lines:
+				build_map(line, dependencies, main)
+			
+			# get file name
+			c_file_name = list(filter(regex.search, c_file.split('/')))[0]
+			
+			# strip the absolute path a level above the file
+			relpath = c_file.split('/')
+			relpath.pop()
+			relpath = '/'.join(relpath)
+			
+			# create object files from .c or .cc/.c++/.cpp
+			objfiles = re.sub('\.c$', '.o', c_file_name)
+			
+			# create a file object
+			fileobject = FileObject(c_file_name, c_file, relpath, objfiles, dependencies, main)
+			# add fileobject to the list of files
+			files.append(fileobject)
+
 	return files
+
+
+def check_file_format(c, cpp):
+	if (len(c) and len(cpp)):
+		print(f'Found both .c and .cc/.c++/.cpp files.\nMakePy currently doesn\'t support compiling C++ programs with C files.')
+		exit()
+	if (not len(c) and not len(cpp)):
+		print(f'No .c or .cc/.c++/.cpp found.')
+		exit()
+	return 'gcc' if len(c) else 'g++'
 
 
 def main():
 	if (len(argv) < 3):
 		exit()
 	direct = argv[2]
+	# check if directory exists
 	if (path.isdir(direct)):
+
+		# gather all relevant files
 		h, c, cpp = grab_files(direct)
-		fobjs = get_dependencies(h, c, cpp)
-		set_makefile('gcc', argv[1], fobjs)
+	
+		# check for c or cpp files
+		compiler = check_file_format(c, cpp)
+		
+		# gather file dependencies
+		fobjs = get_dependencies(compiler, h, c, cpp)
+		
+		# set compiler options and generate makefile
+		set_makefile(compiler, argv[1], fobjs, direct)
 	else:
 		print('Provided path is not a directory.')
 		exit()
